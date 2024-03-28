1a8432e33a3221ca44d06697e56fba61c081070c
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-03-28 15:04:38 -0500

    v0.3 CHANGELOG Added: - backend paths (api, controlleres, middlewares, routes) - validators (middlewares for checking credentials) - API endpoint (/api) - App requires the following dependencies:   -dotenv   -nodemon   -express   -express-validator - Deployable dev environment running 'npm run dev' - Tested using postman on localhost:4000

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,21 @@ require('dotenv').config()
 
 const app = express();
 
+app.use(express.static('../frontend'));
+
+//Lectura y parseo del body
+app.use(express.json());
+
 // Rutas
-app.get('/',(req,ress) => {
-    ress.json({
+app.use('/api/auth',require('./routes/auth.js'));
+
+app.get('/',(req,res) => {
+    res.json({
         ok:true
     })
 })
 
 // Escuchar en puerto 4000
 app.listen(process.env.PORT, () => {
-    console.log('Servidor corriendo en puerto', process.env.PORT)
+    console.log('Servidor corriendo en puerto:', process.env.PORT)
 })
