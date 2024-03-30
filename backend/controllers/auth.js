2b12805083962bf4cc0061eca1b2d0260a76ee69
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-03-29 23:39:42 -0500

    v0.5 CHANGELOG - Added JWT and mongoose as project dependencies

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const usuario = require('../models/usuario');
 const { validationResult } = require('express-validator');
 
@@ -14,6 +15,8 @@ const crearUsuario = async (req, res = express.request ) => {
         }
 
         usuario = new usuario(req.body);
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
         await usuario.save();
 
         res.status(200).json({
@@ -30,10 +33,40 @@ const crearUsuario = async (req, res = express.request ) => {
 }
     
 
-const loginUsuario = (req, res = express.request) => {
+const loginUsuario = async (req, res = express.request) => {
+    const { email, password} = req.body
+
+    try{
+        let usuario = await usuario.findOne({email: email})
+        if (!usuario){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El Usuario no existe'
+            })
+        }
+        
+        const passwordValid = bcrypt.compareSync(password, usuario.password);
+        if (!passwordValid){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password no válido',
+            })
+        }
+        res.status(200).json({
+            ok: true,
+            usuario,
+        })
+    }   catch(error){
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            error,
+        })
+    }
     res.json({
         ok:true
     })
+    
 }
 
 const revalidarToken = (req, res = express.request) => {
