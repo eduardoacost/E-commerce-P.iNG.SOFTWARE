a908c851f3bf46f49186637dee4d62ef1e0b6c3a
eduardoacost <andres_edu.acosta@uao.edu.co>
2024-03-31 15:11:34 -0500

    Navbar+Conexiones a las diferentes paginas

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-import './Navbar.scss';
+import React, { useState } from "react"
+import './Navbar.scss'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from "react-router-dom"
+
+
 const Navbar = () => {
+
+    const [menu , setMenu] = useState('Tienda')
     return (
         <div className="navbar">
-
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>E-COMMERCE</p>
+            </div>
+        <ul className="nav-menu">
+            <li onClick={() => {setMenu('Tienda')}}><Link style={{textDecoration:'none'}} to = '/'>Inicio</Link>{menu === "Tienda"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu('Productos')}}><Link style={{textDecoration:'none'}}to='/Product'>Productos</Link>{menu === "Productos"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu('Contactanos')}}><Link style={{textDecoration:'none'}} to='/Contactanos'>Contactanos</Link>{menu === "Contactanos"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <Link to='/Login'><button>Registrar/Inicio</button></Link>
+            <Link to='/Carrito'><img src={cart_icon} alt="" /></Link>
+            <div className="nav-cart-count">0</div>
+        </div>
         </div>
     )
 }
---
+++ frontend/src/Pages/Contactanos/Contactanos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Contactanos.scss'
+
+const Contactanos = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Contactanos
