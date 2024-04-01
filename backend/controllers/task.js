cc308ef1b24746a39e5b2553a32577f75cebb170
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-04-01 06:12:56 -0500

    v0.7b CHANGELOG - Added problematic task function to main app - Server isn't starting because of this - Need further debugging - Added socket.io dependency - Bugfixes on js files and exporting modules

---
+++ backend/controllers/task.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const Tarea = require('../models/tarea');
+
+const crearTarea = async (req, res = express.request) => {
+    const task = new Tarea (req.body);
+
+    try{
+        task.user = req.uid;
+        const saved = await task.save();
+        res.json({
+            ok: true,
+            task: saved
+        });
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            task: 'Internal error'
+        });
+    }
+}
+
+const listarTareas = async (req, res = express.request) => {
+    const tareas = await Tarea.find().populate('Usuario','nombre');
+
+    try{
+        res.statusCode(200).json({
+            ok: true,
+            tasks
+        });
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Error interno'
+        });
+    }
+}
+
+module.exports = {
+    crearTarea, 
+    listarTareas
+};
