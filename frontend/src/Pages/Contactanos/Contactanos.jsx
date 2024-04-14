078f7790118cde12cd4f1d2b15717af6400ad4bc
eduardoacost <andres_edu.acosta@uao.edu.co>
2024-04-14 01:37:51 -0500

    Se agrego la interfaz de contactanos , del login , el footer ,la del inicio empezada

--- frontend/src/Pages/Contactanos/Contactanos.jsx
+++ frontend/src/Pages/Contactanos/Contactanos.jsx
@@ -3,8 +3,27 @@ import './Contactanos.scss'
 
 const Contactanos = () => {
     return(
-        <div>
-
+        <div className="Contactanos">
+            <p className="cami">Camisetas Colombia</p>
+            <div className="titulo">
+                <p className="form">
+                    Formulario de Contacto
+                </p>
+                <p className="subtitulo">¡Mándanos un Mensaje aqui mismo usando el formulario!</p>
+            </div>
+            <div className="datosmensaje">
+                <p>Nombre Completo</p>
+                <input type="text" />
+                <p>Email</p>
+                <input type="text" />
+            </div>
+            <div className="mensaje">
+                <p>Mensaje</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div className="botonenviar">
+                <button>Enviar</button>
+            </div>
         </div>
     )
 }
--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React , { useState }from "react";
 import './Login.scss';
+import foton from "../../Components/Assets/image 22.png"
+import ojoabierto from "../../Components/Assets/ojo (1).png"
+import ojocerrado from "../../Components/Assets/ojo.png"
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [mostrarContrasena, setMostrarContrasena] = useState(false);
+
+    const toggleMostrarContrasena = () => {
+        setMostrarContrasena(!mostrarContrasena);
+    }
+
     return(
-        <div>
+        <div className="login">
+            <div className="foton">
+                <img src={foton} alt="" />
+            </div>
+            <div className="ti">
+                <p>Camisetas Colombia</p>
+            </div>
+            <div className="subti">
+                <p>Acceso gratuito a todas las herramientas de nuestro E-commerce</p>
+            </div>
+            <div className="info">
+                <p>Email</p>
+                <input type="email" />
+                <p>Contraseña</p>
+                 <div className="contrasena-input">
+                    <input
+                        type={mostrarContrasena ? "text" : "password"}
+                        placeholder=""
+                    />
+                    <img
+                        src={mostrarContrasena ? ojoabierto : ojocerrado}
+                        alt="Mostrar Contraseña"
+                        onClick={toggleMostrarContrasena}
+                    />
+                </div>
+               
+            </div>
+            <div className="btlogon">
+                <button>Iniciar Sesion</button>
+                </div>
+
+            <div className="register">
+            <Link style={{textDecoration:'none'}} to='/Registrar'><p>¿No tienes una cuenta?Registrate</p></Link>
+            </div>
 
         </div>
     )
