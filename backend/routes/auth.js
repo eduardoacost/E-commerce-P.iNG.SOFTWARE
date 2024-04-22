ef076ece0ee922552da3555e4241fdca41e14ffe
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-04-22 00:46:27 -0500

    v0.9.4 - FINALLY: AN ADMIN LOGIN VALIDATION 🔥 - DELETE usuario method - Added extended comments to explain auth routes and controllers' logic

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
-const { crearUsuario, loginUsuario } = require("../controllers/auth");
+const { crearUsuario, loginUsuario, borrarUsuario } = require("../controllers/auth");
 
 const { validarCampos } = require("../middlewares/validarCampos");
 
 router.post("/login", loginUsuario);
+router.delete("/delete", borrarUsuario);
 
 router.post(
   "/new",
   [
     check("username", "username es obligatorio").not().isEmpty(),
     check("identificacion", "Identificación es obligatorio").not().isEmpty(),
-    check("correo", "Formato de correo inválido y es obligatorio")
+    check("correo", "Formato de correo inválido u obligatorio")
       .not()
       .isEmpty()
       .isEmail(),
     check(
       "password",
-      "La contraseña debe ser mínimo de 6 caractéres y es obligatoria"
+      "La contraseña es obligatoria y debe ser mínimo de 6 caractéres"
     ).isLength({ min: 6 }),
     validarCampos,
   ],
